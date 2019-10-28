e0e58e646710e268708fce1b90ba809250d88a00
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2019-10-28 13:25:53 +0100

    autopep8 changes

--- punchie/punchie.py
+++ punchie/punchie.py
@@ -4,106 +4,110 @@ import codecs
 
 
 try:
-	# Try to import Yaml
-	import yaml
+    # Try to import Yaml
+    import yaml
 except ImportError:
-	# If not then install through pip PyYaml
-	import subprocess
-	subprocess.call(['pip', '--quiet', 'install', 'pyyaml'])
-	import yaml
+    # If not then install through pip PyYaml
+    import subprocess
+    subprocess.call(['pip', '--quiet', 'install', 'pyyaml'])
+    import yaml
 
 
 # Punched Card class
 class PunchedCard:
-	def __init__(self, pc):
-		self.content = pc
+    def __init__(self, pc):
+        self.content = pc
 
-	# Print the punched card at the selected path
-	def print_keypunch(self, outpath):
-		outpath = f'Output/{outpath}.puc'
-		try:
-			out_file = codecs.open(outpath, 'w+', encoding='utf8')
-			for j in range(0, 15):
-				line = ''
-				for i in range(0, 67):
-					line += self.content[j,i]
-				out_file.write(f'{line}\n')
-		except IOError:
-			sys.stderr.write(f"Cannot write {outpath}.\n")
+    # Print the punched card at the selected path
+    def print_keypunch(self, outpath):
+        outpath = f'Output/{outpath}.puc'
+        try:
+            out_file = codecs.open(outpath, 'w+', encoding='utf8')
+            for j in range(0, 15):
+                line = ''
+                for i in range(0, 67):
+                    line += self.content[j, i]
+                out_file.write(f'{line}\n')
+        except IOError:
+            sys.stderr.write(f"Cannot write {outpath}.\n")
 
 
 # Holder class that contains Punched Cards
 class Holder:
-	def __init__(self, inpath, form, secret):
-		in_str = get_string(inpath)
-		self.in_name = inpath
-		lines = in_str.split('\n')
-		for line in lines:
-			line = line.replace('\r', '')	# Delete carriege return
-			while len(line) > 0:
-				writen_line = line[0 :64]	# Get the first 64 chars
-				keypunch = PunchedCard(empty_card())
-				try:
-					yaml_reader = codecs.open(f'Yamls/{form}.yaml', 'r', encoding='utf8')
-					yaml_keys = yaml.safe_load(yaml_reader)
-					for i in range(0, len(writen_line)):
-						key = ''
-						j = ''
-						try:
-							if secret == 0:
-								keypunch.content[1, i + 3] = writen_line[i].upper()
-							key = writen_line[i].upper()
-							if key == ' ':
-								pass
-							else:
-								bcd = yaml_keys[f'\\{key}']
-								for j in range(2, 14):
-									if bcd[j - 2] == '_':
-										pass
-									else:
-										keypunch.content[j, i + 3] = u'\u2588'
-										keypunch.content[j, i + 3].encode('utf-8')
-						except KeyError:
-							sys.stderr.write(f"Key error on {key.encode('utf8')} or {j},{i} tuple.\n")
-					self.punched_cards.append(keypunch)
-				except IOError:
-					sys.stderr.write(f"Format {form} not valid.\n")
-				line = line[64 : len(line)]
+    def __init__(self, inpath, form, secret):
+        in_str = get_string(inpath)
+        self.in_name = inpath
+        lines = in_str.split('\n')
+        for line in lines:
+            line = line.replace('\r', '')  # Delete carriege return
+            while len(line) > 0:
+                writen_line = line[0:64]  # Get the first 64 chars
+                keypunch = PunchedCard(empty_card())
+                try:
+                    yaml_reader = codecs.open(
+                        f'Yamls/{form}.yaml', 'r', encoding='utf8')
+                    yaml_keys = yaml.safe_load(yaml_reader)
+                    for i in range(0, len(writen_line)):
+                        key = ''
+                        j = ''
+                        try:
+                            if secret == 0:
+                                keypunch.content[1, i +
+                                                 3] = writen_line[i].upper()
+                            key = writen_line[i].upper()
+                            if key == ' ':
+                                pass
+                            else:
+                                bcd = yaml_keys[f'\\{key}']
+                                for j in range(2, 14):
+                                    if bcd[j - 2] == '_':
+                                        pass
+                                    else:
+                                        keypunch.content[j, i + 3] = u'\u2588'
+                                        keypunch.content[j,
+                                                         i + 3].encode('utf-8')
+                        except KeyError:
+                            sys.stderr.write(
+                                f"Key error on {key.encode('utf8')} or {j},{i} tuple.\n")
+                    self.punched_cards.append(keypunch)
+                except IOError:
+                    sys.stderr.write(f"Format {form} not valid.\n")
+                line = line[64: len(line)]
 
-	# Print all Punched Cards of Holder
-	def write_punched(self):
-		i = 0
-		for keypunch in self.punched_cards:
-			keypunch.print_keypunch(f'{self.in_name}.{str(i)}')
-			i += 1
+    # Print all Punched Cards of Holder
+    def write_punched(self):
+        i = 0
+        for keypunch in self.punched_cards:
+            keypunch.print_keypunch(f'{self.in_name}.{str(i)}')
+            i += 1
 
-	in_name = ''
-	punched_cards = []
+    in_name = ''
+    punched_cards = []
 
 
 # Create an empty punch card dictionary
 def empty_card():
-	in_file = codecs.open('Punched_cards/empty.puc', 'r', encoding='utf8')
-	punched_card = {}
-	y = 0
-	x = 0
-	for line in in_file:
-		x = 0
-		for ch in line:
-			punched_card[y, x] = ch
-			x += 1
-		y += 1
-	return punched_card
+    in_file = codecs.open('Punched_cards/empty.puc', 'r', encoding='utf8')
+    punched_card = {}
+    y = 0
+    x = 0
+    for line in in_file:
+        x = 0
+        for ch in line:
+            punched_card[y, x] = ch
+            x += 1
+        y += 1
+    return punched_card
 
 
 # Get string from file
 def get_string(in_f):
-	if os.path.exists(in_f):
-		in_file = codecs.open(in_f, 'r', encoding='utf8')
-		string = ''
-		for line in in_file:
-			string += line
-		return string
-	else:
-		sys.stderr.write(f"File {in_f} does not exist.\n")
-		return ''
+    if os.path.exists(in_f):
+        in_file = codecs.open(in_f, 'r', encoding='utf8')
+        string = ''
+        for line in in_file:
+            string += line
+        return string
+    else:
+        sys.stderr.write(f"File {in_f} does not exist.\n")
+        return ''
--- punchie/yamlfie.py
+++ punchie/yamlfie.py
@@ -3,72 +3,77 @@ import codecs
 
 # Create all the yaml resource files
 def make_yamls():
-	__make_single_line('cdc')
-	__make_single_line('dec_026')
-	__make_single_line('dec_029')
-	__make_single_line('ge_600')
-	__make_double_line('ibm_024', ['rept', 'prog'])
-	__make_double_line('ibm_026', ['fort', 'comm'])
-	__make_triple_line('ibm_029', ['029', 'ibme', 'ebcd'])
-	__make_single_line('ibm_1401')
-	__make_single_line('univac_1108')
+    __make_single_line('cdc')
+    __make_single_line('dec_026')
+    __make_single_line('dec_029')
+    __make_single_line('ge_600')
+    __make_double_line('ibm_024', ['rept', 'prog'])
+    __make_double_line('ibm_026', ['fort', 'comm'])
+    __make_triple_line('ibm_029', ['029', 'ibme', 'ebcd'])
+    __make_single_line('ibm_1401')
+    __make_single_line('univac_1108')
 
 
 # Create yaml resource file from single puc file
 def __make_single_line(target):
-	in_file = codecs.open(f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
-	x, y, punched_card = __get_puc(in_file)
-	out_file = codecs.open(f'Yamls/{target}.yaml', 'w+', encoding='utf8')
-	__write_yaml(x, y, punched_card, out_file, 0, 1)
+    in_file = codecs.open(
+        f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
+    x, y, punched_card = __get_puc(in_file)
+    out_file = codecs.open(f'Yamls/{target}.yaml', 'w+', encoding='utf8')
+    __write_yaml(x, y, punched_card, out_file, 0, 1)
 
 
 # Create yaml resource file from dual puc file
 def __make_double_line(target, languages):
-	in_file = codecs.open(f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
-	x, y, punched_card = __get_puc(in_file)
-	i = 0
-	for language in languages:
-		out_file = codecs.open(f'Yamls/{target}_{language}.yaml', 'w+', encoding='utf8')
-		__write_yaml(x, y, punched_card, out_file, i, 2)
-		i += 1
+    in_file = codecs.open(
+        f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
+    x, y, punched_card = __get_puc(in_file)
+    i = 0
+    for language in languages:
+        out_file = codecs.open(
+            f'Yamls/{target}_{language}.yaml', 'w+', encoding='utf8')
+        __write_yaml(x, y, punched_card, out_file, i, 2)
+        i += 1
 
 
 # Create yaml resource file from triple puc file
 def __make_triple_line(target, languages):
-	in_file = codecs.open(f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
-	x, y, punched_card = __get_puc(in_file)
-	i = 0
-	for language in languages:
-		out_file = codecs.open(f'Yamls/{target}_{language}.yaml', 'w+', encoding='utf8')
-		__write_yaml(x, y, punched_card, out_file, i, 3)
-		i += 1
+    in_file = codecs.open(
+        f'Punched_cards/{target}.puc', encoding='utf8', mode='r')
+    x, y, punched_card = __get_puc(in_file)
+    i = 0
+    for language in languages:
+        out_file = codecs.open(
+            f'Yamls/{target}_{language}.yaml', 'w+', encoding='utf8')
+        __write_yaml(x, y, punched_card, out_file, i, 3)
+        i += 1
 
 
 # Read from file punch char data and write it on a dictionary
 def __get_puc(in_file):
-	punched_card = {}
-	y = 0
-	x = 0
-	for line in in_file:
-		x = 0
-		for ch in line:
-			if ch != '\n':
-				punched_card[y, x] = ch
-			x += 1
-		y += 1
-	return x, y, punched_card
+    punched_card = {}
+    y = 0
+    x = 0
+    for line in in_file:
+        x = 0
+        for ch in line:
+            if ch != '\n':
+                punched_card[y, x] = ch
+            x += 1
+        y += 1
+    return x, y, punched_card
 
 
 def __write_yaml(x, y, punched_card, out_file, level, contains):
-	for i in range(5, x - 2):
-		key = f'\\{punched_card[level, i]}: '
-		val = ''
-		for j in range(2 + contains, y - 1):
-			if punched_card[j, i] == ' ':
-				val += '_'
-			else:
-				temp = u'\u2588'
-				temp.encode('utf8')
-				val += temp
-		if punched_card[level, i] != ' ':
-			out_file.write(f'{key}\'{val}\'\n')
+    for i in range(5, x - 2):
+        key = f'\\{punched_card[level, i]}: '
+        val = ''
+        for j in range(2 + contains, y - 1):
+            if punched_card[j, i] == ' ':
+                val += '_'
+            else:
+                temp = u'\u2588'
+                temp.encode('utf8')
+                val += temp
+        if punched_card[level, i] != ' ':
+            out_file.write(f'{key}\'{val}\'\n')
